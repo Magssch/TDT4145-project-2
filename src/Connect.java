0a5854a79be070d6e695feb62e2f31f65b0278e4
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2019-03-18 15:28:59 +0100

    qwdq

---
+++ src/Connect.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+import java.util.Properties;
+
+public class Connect {
+
+    public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    protected Connection conn;
+
+    public void connect() {
+        try {
+            Properties p = new Properties();
+            p.put("user", "root");
+            p.put("password", "root");
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/prosjekt2?autoReconnect=true&useSSL=false", p);
+        } catch (Exception e)
+        {
+            throw new RuntimeException("Unable to connect", e);
+        }
+    }
+
+    public void disconnect() {
+        try {
+            conn.close();
+            System.out.println("Closed connection to the database!");
+        } catch(SQLException err) {
+            System.out.println("Unable to disconnect: " + err.toString());
+        }
+    }
+
+
+}
+
---
+++ src/db.sql
@@ -0,0 +1,78 @@
+
+create table Treningsøkt (
+ØktID integer not null,
+Dato date not null,
+Tidspunkt time not null,
+Varighet float(4, 1) not null,
+Notat varchar(100),
+constraint Treningsøkt_PK primary key (ØktID),
+constraint VarighetSjekk check (Varighet > 0));
+
+
+create table Apparat (
+ApparatID integer not null,
+Navn varchar(30) not null,
+Beskrivelse varchar(100) not null,
+constraint Apparat_PK primary key (ApparatID));
+
+
+create table Øvelse (
+ØvelseID integer not null,
+Navn varchar(30) not null,
+PersonligForm integer,
+PersonligPrestasjon integer,
+constraint Øvelse_PK primary key (ØvelseID),
+constraint gyldigForm check (PersonligForm > 0 and PersonligForm <= 10),
+constraint gyldigPrestasjon check (PersonligPrestasjon > 0 and PersonligPrestasjon <= 10));
+
+
+create table Apparatøvelse (
+ØvelseID integer not null,
+Kilo integer not null,
+Sett integer not null,
+ApparatID integer not null,
+constraint Apparatøvelse_FK1 foreign key (ØvelseID) references Øvelse(ØvelseID)
+					on update cascade
+                                        on delete cascade,
+constraint Apparatøvelse_FK2 foreign key (ApparatID) references Apparat(ApparatID)
+					on update cascade
+                                        on delete cascade,
+constraint KiloSjekk check (Kilo > 0),
+constraint SettSjekk check (Sett > 0));
+
+
+create table IkkeApparatøvelse (
+ØvelseID integer not null,
+Beskrivelse varchar(100) not null,
+constraint IkkeApparatøvelse_FK foreign key (ØvelseID) references Øvelse(ØvelseID)
+					on update cascade
+                                        on delete cascade);
+
+
+create table ØvelseIØkt (
+ØktID integer not null,
+ØvelseID integer not null,
+constraint ØvelseIØkt_FK1 foreign key (ØktID) references Treningsøkt(ØktID)
+					on update cascade
+                                        on delete cascade,
+constraint ØvelseIØkt_FK2 foreign key (ØvelseID) references Øvelse(ØvelseID)
+					on update cascade
+                                        on delete cascade);
+
+
+create table Øvelsesgruppe (
+GruppeID integer not null,
+Navn varchar(30) not null,
+constraint Øvelsesgruppe_PK primary key (GruppeID));
+
+
+create table ØvelseIGruppe (
+ØvelseID integer not null,
+GruppeID integer not null,
+constraint ØvelseIGruppe_FK1 foreign key (ØvelseID) references Øvelse(ØvelseID)
+					on update cascade
+                                        on delete cascade,
+constraint ØvelseIGruppe_FK2 foreign key (Gruppe) references Øvelsesgruppe(GruppeID)
+					on update cascade
+                                        on delete cascade);
+
